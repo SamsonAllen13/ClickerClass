9498af77e44eb012bf272705f694bdce069be97a
direwolf420 <amakejk1@web.de>
2020-12-08 21:15:24 +0100

    add LangHelper, UpdateLocalizationFiles

---
+++ Utilities/LangHelper.cs
@@ -0,0 +1,26 @@
+﻿using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ClickerClass.Utilities
+{
+	public static class LangHelper
+	{
+		/// <summary>
+		/// prefixes the modname for the key
+		/// </summary>
+		/// <returns>Text associated with this key</returns>
+		public static string GetTextByMod(Mod mod, string key, params object[] args)
+		{
+			return Language.GetTextValue($"Mods.{mod.Name}.{key}", args);
+		}
+
+		/// <summary>
+		/// Defaults to Mods.ClickerClass. as the prefix for the key
+		/// </summary>
+		/// <returns>Text associated with this key</returns>
+		internal static string GetText(string key, params object[] args)
+		{
+			return GetTextByMod(ClickerClass.mod, key, args);
+		}
+	}
+}

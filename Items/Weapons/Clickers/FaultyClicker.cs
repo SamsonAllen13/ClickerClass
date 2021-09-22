bd06d276025fdb7831875bdc3273d1777a0cb2c7
direwolf420 <amakejk1@web.de>
2021-09-22 13:34:57 +0200

    clean up FaultyClicker effect

--- Items/Weapons/Clickers/FaultyClicker.cs
+++ Items/Weapons/Clickers/FaultyClicker.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.DataStructures;
-using System;
 
 namespace ClickerClass.Items.Weapons.Clickers
 {
@@ -22,20 +21,24 @@ namespace ClickerClass.Items.Weapons.Clickers
 				{
 					radius = 350;
 				}
-				
-				//Hopefully this doesnt offend dire
-				double xOffset = 0;
-				double yOffset = 0;
-				while (!Collision.CanHit(new Vector2(player.Center.X, player.Center.Y - 12), 1, 1, new Vector2((float)xOffset, (float)yOffset), 1, 1))
+
+				Vector2 offset = player.Center;
+				int tries = 0;
+				do
 				{
-					//Circles give me a damn headache...
-					double r = radius * Math.Sqrt(Main.rand.NextFloat(0f, 1f));
-					double theta = Main.rand.NextFloat(0f, 1f) * MathHelper.TwoPi;
-					xOffset = player.Center.X + r * Math.Cos(theta);
-					yOffset = player.Center.Y + r * Math.Sin(theta);
+					offset = Vector2.UnitY.RotatedByRandom(MathHelper.TwoPi);
+					offset *= radius * Main.rand.NextFloat();
+					offset += player.Center;
+					tries++;
+				}
+				while (tries < 100 && !Collision.CanHit(new Vector2(player.Center.X, player.Center.Y - 12), 1, 1, offset, 1, 1));
+
+				if (tries >= 100)
+				{
+					return;
 				}
 
-				Projectile.NewProjectile(source, (float)xOffset, (float)yOffset, 0f, 0f, ModContent.ProjectileType<FaultyClickerPro>(), (int)(damage * 1.5f), 0f, player.whoAmI);
+				Projectile.NewProjectile(source, offset, Vector2.Zero, ModContent.ProjectileType<FaultyClickerPro>(), (int)(damage * 1.5f), 0f, player.whoAmI);
 			});
 		}
 

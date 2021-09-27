981d38d02dcc76613aebc1e102bea933e9d47c3e
direwolf420 <amakejk1@web.de>
2021-09-27 13:06:24 +0200

    get rid of duplicate crit buffs

--- Buffs/ClickerGlobalBuff.cs
+++ Buffs/ClickerGlobalBuff.cs
@@ -6,19 +6,6 @@ namespace ClickerClass.Buffs
 {
 	public class ClickerGlobalBuff : GlobalBuff
 	{
-		public override void Update(int type, Player player, ref int buffIndex)
-		{
-			ref var crit = ref player.GetCritChance<ClickerDamage>();
-			switch (type)
-			{
-				case BuffID.WellFed:
-					crit += 2;
-					break;
 
-				case BuffID.Rage:
-					crit += 10;
-					break;
-			}
-		}
 	}
 }

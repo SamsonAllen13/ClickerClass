2fa6142e351657ab51f0a68937b4d2e1ed50bde8
direwolf420 <amakejk1@web.de>
2021-12-06 16:58:13 +0100

    LongerExpertDebuff fix from tml update

--- Buffs/Embrittle.cs
+++ Buffs/Embrittle.cs
@@ -1,5 +1,6 @@
 using ClickerClass.NPCs;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ClickerClass.Buffs
@@ -9,7 +10,7 @@ namespace ClickerClass.Buffs
 		public override void SetStaticDefaults()
 		{
 			Main.debuff[Type] = true;
-			LongerExpertDebuff = true;
+			BuffID.Sets.LongerExpertDebuff[Type] = true;
 		}
 
 		public override void Update(NPC npc, ref int buffIndex)

260e2a23ce84a6bd9bcec56fe2c448edbcc5c4e5
direwolf420 <amakejk1@web.de>
2021-12-10 23:07:17 +0100

    add modified vanilla drop code for normal/expert

---
+++ DropRules/CommonDropWithReroll.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+
+namespace ClickerClass.DropRules
+{
+	public class CommonDropWithReroll : CommonDrop
+	{
+		readonly float rerollChance;
+
+		//Make all spawn chances more likely on expert mode
+		//Formula:
+		//expertmode% = normalmode% * (1 + (1 - normalmode%) * rerollChance)
+		//Mimics CommonDropWithRerolls but with custom % instead of #rerolls
+		public CommonDropWithReroll(int itemId, int chanceDenominator = 1, int amountDroppedMinimum = 1, int amountDroppedMaximum = 1, int chanceNumerator = 1, float rerollChance = 0.5f)
+			: base(itemId, chanceDenominator, amountDroppedMinimum, amountDroppedMaximum, chanceNumerator) {
+			this.rerollChance = rerollChance;
+		}
+
+		public override ItemDropAttemptResult TryDroppingItem(DropAttemptInfo info)
+		{
+			bool flag = Roll(info);
+			if (!flag && info.rng.NextFloat() < rerollChance)
+			{
+				flag = Roll(info);
+			}
+
+			ItemDropAttemptResult result;
+			if (flag)
+			{
+				CommonCode.DropItemFromNPC(info.npc, itemId, info.rng.Next(amountDroppedMinimum, amountDroppedMaximum + 1));
+				result = default(ItemDropAttemptResult);
+				result.State = ItemDropAttemptResultState.Success;
+				return result;
+			}
+
+			result = default(ItemDropAttemptResult);
+			result.State = ItemDropAttemptResultState.FailedRandomRoll;
+			return result;
+
+			bool Roll(DropAttemptInfo info)
+			{
+				return info.player.RollLuck(chanceDenominator) < chanceNumerator;
+			}
+		}
+
+		public override void ReportDroprates(List<DropRateInfo> drops, DropRateInfoChainFeed ratesInfo) {
+			float chance = chanceNumerator / (float)chanceDenominator;
+			float inverseChance = 1f - chance;
+
+			float finalChance = chance; //First roll could be a success
+			finalChance += inverseChance * rerollChance * chance; //Other success is when first roll fails, reroll happens, and it rolls a success
+
+			float dropRate = finalChance * ratesInfo.parentDroprateChance;
+			drops.Add(new DropRateInfo(itemId, amountDroppedMinimum, amountDroppedMaximum, dropRate, ratesInfo.conditions));
+			Chains.ReportDroprates(base.ChainedRules, finalChance, drops, ratesInfo);
+		}
+	}
+}
---
+++ Utilities/DropHelper.cs
@@ -0,0 +1,66 @@
+﻿using ClickerClass.DropRules;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ModLoader;
+
+namespace ClickerClass.Utilities
+{
+	internal static class DropHelper
+	{
+		//Mimics ItemDropRule.ExpertGetsRerolls but with custom % instead of #rerolls
+		//Wrapper for other rules ontop
+		internal static void NPCExpertGetsRerolls(NPCLoot npcLoot, int itemId, int chanceDenominator = 1, int minimumDropped = 1, int maximumDropped = 1, int chanceNumerator = 1, IItemDropRule ruleExpert = null, IItemDropRule ruleNormal = null, float rerollChance = 0.5f)
+		{
+			//Since the conditions are exclusive, only one of them will show up
+			IItemDropRule expertRule = new LeadingConditionRule(new Conditions.IsExpert());
+			IItemDropRule ruleToAdd = expertRule;
+			if (ruleExpert != null)
+			{
+				ruleToAdd = ruleExpert; //If a rule is specified, use that to add it (Always add the "baseline" rule first)
+				expertRule = ruleToAdd.OnSuccess(expertRule);
+			}
+			expertRule.OnSuccess(new CommonDropWithReroll(itemId, chanceDenominator, minimumDropped, maximumDropped, chanceNumerator, rerollChance));
+			npcLoot.Add(ruleToAdd);
+
+			//Vanilla example
+			//Conditions.IsPumpkinMoon condition2 = new Conditions.IsPumpkinMoon();
+			//Conditions.FromCertainWaveAndAbove condition3 = new Conditions.FromCertainWaveAndAbove(15);
+
+			//LeadingConditionRule entry = new LeadingConditionRule(condition2);
+			//LeadingConditionRule ruleToChain = new LeadingConditionRule(condition3);
+			//npcLoot.Add(entry).OnSuccess(ruleToChain).OnSuccess(ItemDropRule.Common(1856));
+
+			IItemDropRule notExpertRule = new LeadingConditionRule(new Conditions.NotExpert());
+			ruleToAdd = notExpertRule;
+			if (ruleNormal != null)
+			{
+				ruleToAdd = ruleNormal;
+				notExpertRule = ruleToAdd.OnSuccess(notExpertRule);
+			}
+			notExpertRule.OnSuccess(new CommonDrop(itemId, chanceDenominator, minimumDropped, maximumDropped, chanceNumerator));
+			npcLoot.Add(ruleToAdd);
+		}
+
+		internal static void GlobalExpertGetsRerolls(GlobalLoot globalLoot, int itemId, int chanceDenominator = 1, int minimumDropped = 1, int maximumDropped = 1, int chanceNumerator = 1, IItemDropRule ruleExpert = null, IItemDropRule ruleNormal = null)
+		{
+			IItemDropRule expertRule = new LeadingConditionRule(new Conditions.IsExpert());
+			IItemDropRule ruleToAdd = expertRule;
+			if (ruleExpert != null)
+			{
+				ruleToAdd = ruleExpert; //If a rule is specified, use that to add it (Always add the "baseline" rule first)
+				expertRule = ruleToAdd.OnSuccess(expertRule);
+			}
+			expertRule.OnSuccess(new CommonDropWithReroll(itemId, chanceDenominator, minimumDropped, maximumDropped, chanceNumerator));
+			globalLoot.Add(ruleToAdd);
+
+			IItemDropRule notExpertRule = new LeadingConditionRule(new Conditions.NotExpert());
+			ruleToAdd = notExpertRule;
+			if (ruleNormal != null)
+			{
+				ruleToAdd = ruleNormal;
+				notExpertRule = ruleToAdd.OnSuccess(notExpertRule);
+			}
+			notExpertRule.OnSuccess(new CommonDrop(itemId, chanceDenominator, minimumDropped, maximumDropped, chanceNumerator));
+			globalLoot.Add(ruleToAdd);
+		}
+	}
+}

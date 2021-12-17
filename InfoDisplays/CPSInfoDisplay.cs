62958da24fddd2dc17d7b0c178116dbde3dbe247
direwolf420 <amakejk1@web.de>
2021-12-17 12:39:38 +0100

    Preliminary CPS Info Display implementation, needs accessory and icon

---
+++ InfoDisplays/CPSInfoDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ClickerClass.InfoDisplays
+{
+	public class CPSInfoDisplay : InfoDisplay
+	{
+		public override bool Active() {
+			return true; //TODO Usually some accessory flag here
+		}
+
+		public override string DisplayValue() {
+			Player player = Main.LocalPlayer;
+			return "" + Math.Floor(player.GetModPlayer<ClickerPlayer>().clickerPerSecond) + " Clicks per second";
+		}
+	}
+}

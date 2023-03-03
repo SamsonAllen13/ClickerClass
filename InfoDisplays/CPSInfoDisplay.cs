a763927d4dd3b64b6145627719ea91934cf7de77
direwolf420 <amakejk1@web.de>
2023-03-03 12:43:56 +0100

    (doesnt work) CPS info pluralization

--- InfoDisplays/CPSInfoDisplay.cs
+++ InfoDisplays/CPSInfoDisplay.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using Terraria;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace ClickerClass.InfoDisplays
 {
 	public class CPSInfoDisplay : InfoDisplay
 	{
+		public LocalizedText DisplayValueText;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayValueText = this.GetLocalization("DisplayValue");
+		}
+
 		public override bool Active() {
 			Player player = Main.LocalPlayer;
 			ClickerPlayer clickerPlayer = player.GetModPlayer<ClickerPlayer>();
@@ -21,7 +29,8 @@ namespace ClickerClass.InfoDisplays
 			{
 				displayColor = InactiveInfoTextColor;
 			}
-			return $"{val} {DisplayName}";
+			//TODO currently doesn't work, crashes. both "{$Mods.ClickerClass.Common.Tooltips.Clicks} per second" and "{0} {^0:click;clicks} per second". no pluralization works however
+			return DisplayValueText.WithFormatArgs(val).ToString();
 		}
 	}
 }

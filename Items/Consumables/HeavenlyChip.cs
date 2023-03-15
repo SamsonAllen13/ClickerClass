871605c4a022c284ab4ca7b3f8dd121ae116aba8
direwolf420 <amakejk1@web.de>
2023-03-15 15:57:13 +0100

    remove heavenly chip reverter

--- Items/Consumables/HeavenlyChip.cs
+++ Items/Consumables/HeavenlyChip.cs
@@ -1,6 +1,5 @@
 ﻿using Terraria;
 using Terraria.ID;
-using Terraria.ModLoader;
 
 namespace ClickerClass.Items.Consumables
 {
@@ -28,52 +27,16 @@ namespace ClickerClass.Items.Consumables
 		public override bool CanUseItem(Player player)
 		{
 			ClickerPlayer clickerPlayer = player.GetModPlayer<ClickerPlayer>();
-			return (!clickerPlayer.consumedHeavenlyChip);
+			return !clickerPlayer.consumedHeavenlyChip;
 		}
 
 		public override bool? UseItem(Player player)
 		{
-			if (player.itemAnimation > 0 && player.itemTime == 0)
+			if (player.itemAnimation > 0 && player.ItemTimeIsZero)
 			{
 				ClickerPlayer clickerPlayer = player.GetModPlayer<ClickerPlayer>();
 				clickerPlayer.consumedHeavenlyChip = true;
-				return true;
-			}
-
-			return null;
-		}
-	}
-
-	// FOR TESTING ONLY
-	public class HeavenlyChipReverter : ModItem
-	{
-		public override string Texture => "ClickerClass/Items/Consumables/HeavenlyChip";
-
-		public override void SetStaticDefaults()
-		{
-			base.SetStaticDefaults();
-			Item.ResearchUnlockCount = 0;
-		}
-
-		public override void SetDefaults()
-		{
-			Item.width = 28;
-			Item.height = 28;
-			Item.maxStack = Item.CommonMaxStack;
-			Item.consumable = true;
-			Item.useTime = 45;
-			Item.useAnimation = 45;
-			Item.UseSound = SoundID.Item92;
-			Item.useStyle = ItemUseStyleID.HoldUp;
-			Item.rare = ItemRarityID.Gray;
-		}
-
-		public override bool? UseItem(Player player)
-		{
-			if (player.itemAnimation > 0 && player.itemTime == 0)
-			{
-				ClickerPlayer clickerPlayer = player.GetModPlayer<ClickerPlayer>();
-				clickerPlayer.consumedHeavenlyChip = false;
+				//No syncing required, click radius is not something that is seen or interacted with by other players
 				return true;
 			}
 

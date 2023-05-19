4246c8666aba06e9bede6afdf1f4d9bd0b4e24dd
direwolf420 <amakejk1@web.de>
2023-05-19 11:15:30 +0200

    remove Devour effect references

--- ClickEffect.cs
+++ ClickEffect.cs
@@ -157,7 +157,6 @@ namespace ClickerClass
 		public static string Mania { get; internal set; } = string.Empty;
 		public static string Nab { get; internal set; } = string.Empty;
 		public static string PartyTime { get; internal set; } = string.Empty;
-		public static string Devour { get; internal set; } = string.Empty;
 		public static string StarSlice { get; internal set; } = string.Empty;
 		public static string Evolve { get; internal set; } = string.Empty;
 		public static string HappyHeart { get; internal set; } = string.Empty;

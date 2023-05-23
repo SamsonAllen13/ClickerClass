d6b00fbe31dab91da97c72d607393dd55d468531
direwolf420 <amakejk1@web.de>
2023-05-23 13:48:11 +0200

    localize configs with tml help

--- ClickerConfig.cs
+++ ClickerConfig.cs
@@ -3,7 +3,6 @@ using Terraria.ModLoader.Config;
 
 namespace ClickerClass
 {
-	[Label("$Mods.ClickerClass.ClickerConfigClient.Label")]
 	public class ClickerConfigClient : ModConfig
 	{
 		public override ConfigScope Mode => ConfigScope.ClientSide;
@@ -11,38 +10,26 @@ namespace ClickerClass
 		// Automatically set by tModLoader
 		public static ClickerConfigClient Instance;
 
-		[Header("$Mods.ClickerClass.ClickerConfigClient.Header.DisplayOptions")]
+		[Header("DisplayOptions")]
 
-		[Label("$Mods.ClickerClass.ClickerConfigClient.ShowClassTags.Label")]
-		[Tooltip("$Mods.ClickerClass.ClickerConfigClient.ShowClassTags.Tooltip")]
 		[DefaultValue(true)]
 		public bool ShowClassTags;
 		
-		[Label("$Mods.ClickerClass.ClickerConfigClient.ShowEffectSuggestion.Label")]
-		[Tooltip("$Mods.ClickerClass.ClickerConfigClient.ShowEffectSuggestion.Tooltip")]
 		[DefaultValue(true)]
 		public bool ShowEffectSuggestion;
 
-		[Label("$Mods.ClickerClass.ClickerConfigClient.ShowCustomCursors.Label")]
-		[Tooltip("$Mods.ClickerClass.ClickerConfigClient.ShowCustomCursors.Tooltip")]
 		[DefaultValue(true)]
 		public bool ShowCustomCursors;
 
-		[Label("$Mods.ClickerClass.ClickerConfigClient.ShowClickIndicator.Label")]
-		[Tooltip("$Mods.ClickerClass.ClickerConfigClient.ShowClickIndicator.Tooltip")]
 		[DefaultValue(false)]
 		public bool ShowClickIndicator;
 
-		[Label("$Mods.ClickerClass.ClickerConfigClient.ShowOthersClickIndicator.Label")]
-		[Tooltip("$Mods.ClickerClass.ClickerConfigClient.ShowOthersClickIndicator.Tooltip")]
 		[DefaultValue(true)]
 		public bool ShowOthersClickIndicator;
 		
-		[Header("$Mods.ClickerClass.ClickerConfigClient.Header.GameplayOptions")]
+		[Header("GameplayOptions")]
 		
 		[BackgroundColor(255, 175, 100)]
-		[Label("$Mods.ClickerClass.ClickerConfigClient.ToggleAutoreuseLimiter.Label")]
-		[Tooltip("$Mods.ClickerClass.ClickerConfigClient.ToggleAutoreuseLimiter.Tooltip")]
 		[DefaultValue(true)]
 		public bool ToggleAutoreuseLimiter;
 	}

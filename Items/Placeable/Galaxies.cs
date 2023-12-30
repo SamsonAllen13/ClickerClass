0d6c109135c14edb27ac55cdeecef2b0935d67ae
Barometz-Mod <barometz.thorium@gmail.com>
2023-12-30 23:10:19 +0800

    Add paintings (unobtainable)

--- Items/Placeable/Galaxies.cs
+++ Items/Placeable/Galaxies.cs
@@ -1,15 +1,15 @@
-using ClickerClass.Tiles;
+﻿using ClickerClass.Tiles;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ClickerClass.Items.Placeable
 {
-	public class OutsideTheCave : ModItem
+	public class Galaxies : ModItem
 	{
 		public override void SetDefaults()
 		{
-			Item.DefaultToPlaceableTile(ModContent.TileType<Painting4x3>(), 0); //tile, style
+			Item.DefaultToPlaceableTile(ModContent.TileType<Painting6x4>(), 0); //tile, style
 			Item.width = 30;
 			Item.height = 30;
 			Item.value = Item.sellPrice(0, 0, 20, 0);
